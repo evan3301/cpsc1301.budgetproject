55d2aaaf17ef96ec69b0413926abdef8b7edfc0f
Evan Ahmed <137008996+evan3301@users.noreply.github.com>
2023-10-24 20:42:13 -0400

    Add files via upload
    Pseudo-code outline

---
+++ README.py
@@ -0,0 +1,49 @@
+'''
+Code concept:
+    Personal budget generator:
+        pre-defined costs
+        pre-defined tax brackets
+            usr enter personal info:
+                est. annual salary
+                est. monthly car payments
+                est. monthly home payments
+
+        generate budget recommendations:
+            entertainment
+            groceries
+            savings
+            retirments
+            misc
+'''
+
+'''
+Code implement:
+
+class Costs:
+    methods = [
+                def __init__(self):
+
+
+                def preDef(self, ...):
+                    categories = {
+                        'Taxes': ['Federal', 'State']
+                        'Housing': ['Rent', 'Mortgage', 'Utilities', 'Maintenence']
+                        'Car': ['Gas', 'Car payments', 'Maintenence']
+                    }
+
+
+                def usrInputInfo(self, ...):
+                    
+
+                def usrInputCosts(self, ...):
+
+
+                def generateCosts(self, ...):
+
+
+                def calcBudgetRec(self, ...):
+
+
+                def outputReprompt(self, ...):
+    ]
+'''

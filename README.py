f3eb7b801c8dbfc625437c578831f32d4555fd14
Evan Ahmed <137008996+evan3301@users.noreply.github.com>
2023-10-24 23:19:46 -0400

    Update README.py

--- README.py
+++ README.py
@@ -2,48 +2,24 @@
 Code concept:
     Personal budget generator:
         pre-defined costs
+            category: percentage dictionary
         pre-defined tax brackets
-            usr enter personal info:
-                est. annual salary
-                est. monthly car payments
-                est. monthly home payments
+            state: tax dictionary for every state
+            
+        usr enter personal info:
+            state
+            est. annual salary
 
-        generate budget recommendations:
+        calculation method:
+            usr salary post-tax
+            post-tax salary * category calculations
+            
+            new budget dictionary to flood calculations
+
+        generate & display budget recommendations:
             entertainment
             groceries
             savings
             retirments
             misc
-'''
-
-'''
-Code implement:
-
-class Costs:
-    methods = [
-                def __init__(self):
-
-
-                def preDef(self, ...):
-                    categories = {
-                        'Taxes': ['Federal', 'State']
-                        'Housing': ['Rent', 'Mortgage', 'Utilities', 'Maintenence']
-                        'Car': ['Gas', 'Car payments', 'Maintenence']
-                    }
-
-
-                def usrInputInfo(self, ...):
-                    
-
-                def usrInputCosts(self, ...):
-
-
-                def generateCosts(self, ...):
-
-
-                def calcBudgetRec(self, ...):
-
-
-                def outputReprompt(self, ...):
-    ]
 '''
